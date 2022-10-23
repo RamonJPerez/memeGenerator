91159d2a582977bd4e35f2b384df863265ea3d34
Ramon <104288305+RamonJPerez@users.noreply.github.com>
2022-10-22 22:19:12 -0400

    first memegenerator garmin

---
+++ memeGenerator/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+      src="./src/images/troll-face.png"
+      className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 classname="header--project">React Course - project 3</h4>
+    </header>
+         )
+}
